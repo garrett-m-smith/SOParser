586067f744236f7b8eff319eed7b9484a0cffbf1
Garrett Smith <garrett.smith@uconn.edu>
2017-05-04 11:18:22 -0400

    Add reading in lexicon from YAML file. Seems to work setting up the complete lexicon! Next step, setting up the dynamics and testing.

--- TreeletActivation/TreeletInfrastructure.py
+++ TreeletActivation/TreeletInfrastructure.py
@@ -10,48 +10,44 @@ This script will be used to set up Node and Treelet classes with associated
 properties, methods, and algorithms
 """
 
+import yaml
+
 class Node(object):
     """Basic structure for nodes/attachment sites on a treelet."""
-    def __init__(self, attch_name, feature_vector):
-        self.feature_vector = feature_vector
-        # Links should be a list of lists 
-        self.links = []
-        # Give attch. site name, z.B., 'DO', 'mother'
-        self.name = attch_name
+    def __init__(self, attch_dict):
+        self.feature_vector = list(attch_dict.values())
+        self.name = list(attch_dict.keys())[0]
         
     def get_feature_vector(self):
         return self.feature_vector
-    
-    def add_link(self, mother, daughter_treelet, daughter_site):
-        """Indexing scheme: mother treelet, daughter treelet, daughter
-        attachment site."""
-        self.links.append([mother, daughter_treelet, daughter_site])
 
 
 class Treelet(object):
     """A container for all of the nodes in a treelet. Consists of a mother
     node and a list of daughter nodes."""
-    def __init__(self, phon_form, mother_feat, daughters):
-        # daughters arg should be list of lists: ea. entry has a name and a
+    def __init__(self, name, input_dict):
+        # daughters arg should be dict of dicts: ea. entry has a name and a
         # feat. vec.
-        self.phon_form = phon_form
-        self.name = phon_form
+        self.phon_form = input_dict['phon_form']
+        self.name = name
         
         # Init activation & threshold
         self.activation = 0.01
         self.threshold = 0.1
         
         # Making the mother node
-        self.mother = Node('mother', mother_feat)
+        self.mother = Node({'mother': input_dict['mother_feat']})
         
-        # Making the list for the daughters
+        # Making the dict for the daughters
         self.daughters = {}
         
         # Adding daughter attachment sites
-        for attch in range(len(daughters)):
-            curr = daughters[attch]
-            new_attch = Node(curr[0], curr[1])
-            self.daughters.update({new_attch.name: new_attch})
+        daughters = input_dict['daughters']
+        if daughters is not None:
+            for attch in daughters:
+                curr = {attch: daughters[attch]}
+                new_attch = Node(curr)
+                self.daughters.update({new_attch.name: new_attch})
     
     def get_activation(self):
         return self.activation
@@ -61,23 +57,25 @@ class Treelet(object):
 
 
 class Lexicon(object):
-    """Container for Treelets and links, including dynamics."""
+    """Container for Treelets and links, possibly including dynamics."""
     
     def __init__(self):
         # The list of all Treelet objects in the lexicon
         self.treelets = {}
         self.ntreelets = len(self.treelets)
         self.links = {}
+        self.nlinks = len(self.treelets)
         
     def add_treelet(self, *args):
         """Function for adding a new treelet to the lexicon. Checks if the
         treelet (a phon., mother, daughters pairing) is already in the
         lexicon. Assumes that to-be-added treelet has a unique name."""
         for new_treelet in args:
-            if new_treelet.phon_form not in self.treelets:
-                self.treelets.update({new_treelet.phon_form: new_treelet})
+            if new_treelet.name not in self.treelets:
+                self.treelets.update({new_treelet.name: new_treelet})
+        self.ntreelets += 1
     
-    def make_links(self):
+    def _make_links(self):
         """Links are stored in nested dictionaries, so referencing them will
         look like this: 
             lex.links[dependent_treelet][head_treelet][head_daughter].
@@ -88,17 +86,32 @@ class Lexicon(object):
             for head in others:
                 for attch in self.treelets[head].daughters:
                     self.links.setdefault(dep, {}).setdefault(head, {}).setdefault(attch, 0)
+                    self.nlinks += 1
     
-    def import_treelets(self):
+    def _import_treelets(self, file):
+        """Imports the lexicon from a YAML file."""
+        with open(file, 'r') as stream:
+            imported_lex = yaml.safe_load(stream)
+        for word in imported_lex:
+            curr = imported_lex[word]
+            new_treelet = Treelet(word, curr)
+            self.add_treelet(new_treelet)
+            
+    def build_lexicon(self, file):
+        self._import_treelets(file)
+        self._make_links()
         
+    # Later, possibly add plotting methods from NetworkX to make figures
+    # showing the links in the form of a directed graph!
 
 
 if __name__ == '__main__':
-    the = Treelet('the', [1, 0, 1, 0], [])
-    dog = Treelet('dog', [0, 1, 0, 1], [['Det', [1, 0, 1, 0]], ['Adj', [0, 0, 1, 1]]])
-    eats = Treelet('eats', [0, 0, 0, 0], [['Subj', [0, 1, 0, 1]], ['DO', [0, 1, 0, 1]]])
+#    the = Treelet('the', [1, 0, 1, 0], [])
+#    dog = Treelet('dog', [0, 1, 0, 1], [['Det', [1, 0, 1, 0]], ['Adj', [0, 0, 1, 1]]])
+#    eats = Treelet('eats', [0, 0, 0, 0], [['Subj', [0, 1, 0, 1]], ['DO', [0, 1, 0, 1]]])
     
     lex = Lexicon()
-    lex.add_treelet(the, dog, eats)
-    lex.make_links()
+#    lex.add_treelet(the, dog, eats)
+#    lex.make_links()
+    lex.build_lexicon('Lexicon.yaml')
     
