dc9eb6b60a17761bb31ad829e175488eeacb101f
Garrett Smith <garrett.smith@uconn.edu>
2017-07-18 15:26:40 -0400

    Updated individual runs script to use same params as the MC version. Individual runs look pretty good, although it's interesting to note that one link often takes a very long time to win...

--- PsPartPaperModel/IndividualRuns.py
+++ PsPartPaperModel/IndividualRuns.py
@@ -9,7 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 nlinks = 6
-k = 2
+#k = 2
+k = 1.1
 W = np.array([[1, k, 0, k, 0, k],
               [k, 1, k, 0, k, 0],
               [0, k, 1, k, 0, k],
@@ -30,39 +31,56 @@ ipt = box_of_N2 # Container
 #ipt = lot_of_N2 # Measure Phrase
 
 tau = 0.01
-nsec = 50
-tvec = np.linspace(0, nsec, nsec/tau + 1)
-x0 = np.array([0.2] * nlinks)
+ntsteps = 10000
+#nsec = 50
+#tvec = np.linspace(0, nsec, nsec/tau + 1)
+#x0 = np.array([0.2] * nlinks)
+#adj = 2.
+
+x0 = np.array([0.001] * nlinks)
 adj = 2.
 
 # Setting first word to between its current state and 1
-x0[0] = x0[0] + (1 - x0[0]) / adj
+#x0[0] = x0[0] + (1 - x0[0]) / adj
+#x0[0] = 0.01
+x0[0] = 0.1
   
 # Creating history fector and initializing noise
-xhist = np.zeros((len(tvec), nlinks))
+xhist = np.zeros((ntsteps, nlinks))
 xhist[0,] = x0
-noisemag = 1.5
+#noisemag = 1.
+noisemag = 0.5
 noise = np.random.normal(0, noisemag, xhist.shape)
 
+# Length manipulation
+length = 0
+# length = 1
+
+if length == 0:
+    # Half the boost if short
+    adj = 0.05
+else:
+    adj = 0.1
+    
+# N1 Type manipulation
+#ipt = box_of_N2
+ipt = group_of_N2
+#ipt = lot_of_N2
 
 # Individual runs
-for t in range(1, len(tvec)):
-    xhist[t,:] = np.clip(xhist[t-1,] + tau * (xhist[t-1,] 
-    * (ipt - W @ (ipt * xhist[t-1,])) + noise[t,:]), -0.1, 1.1)
+for t in range(1, ntsteps):
+                # Euler forward dynamics
+                xhist[t,:] = np.clip(xhist[t-1,] + tau * (xhist[t-1,] 
+                * (ipt - W @ (ipt * xhist[t-1,])) + noise[t,:]), -0.1, 1.1)
+#                xhist[t,:] = np.clip(xhist[t-1,] + tau * (xhist[t-1,] 
+#                * (ipt - W @ (ipt * xhist[t-1,]) + noise[t,:])), -0.1, 1.1)
     
-    # Introducing the preposition
-    if t == 100:
-        # Turn boost P-P(N1) = intro Prep
-        xhist[t,2] = xhist[t,2] + (1 - xhist[t,2]) / adj
-        # Also turn on its competitor: N1-N(P)
-        xhist[t,1] = xhist[t,1] + (1 - xhist[t,1]) / adj
-    if t == 200:
-        # Intro N2-related links: P-Det(N2)
-        xhist[t,3] = xhist[t,3] + (1 - xhist[t,3]) / adj
-        # N2-N(P)
-        xhist[t,4] = xhist[t,4] + (1 - xhist[t,4]) / adj
-        # N2-N(V)
-        xhist[t,4] = xhist[t,5] + (1 - xhist[t,5]) / adj
+                if t == 25:
+                    xhist[t,2] += adj
+                    xhist[t,1] += adj
+                if t == 50:
+                    xhist[t,3:] += adj
+
 
 # If you want to save individual trajectories as CSVs
 #np.savetxt('box-N1-headed.csv', xhist.T, delimiter = ',', fmt = '%6f')
@@ -78,9 +96,9 @@ plt.figure()
 plt.ylim(-0.1, 1.1)
 for d in range(xhist.shape[1]):
     plt.plot(xhist[:,d], label = link_names[d])
-    xpos = d * (len(xhist[:,d]) / len(link_names))
-    ypos = xhist[xpos, d]
-    plt.text(xpos, ypos, link_names[d])
+#    xpos = d * (len(xhist[:,d]) / len(link_names))
+#    ypos = xhist[xpos, d]
+#    plt.text(xpos, ypos, link_names[d])
 plt.legend()
 plt.title('Individual link competition run')
 plt.show()
