54f6ee13457a5f36f79bddf609d31d14adc1f374
Garrett Smith <garrett.smith@uconn.edu>
2017-08-04 11:03:29 -0400

    Update.

--- PsPartPaperModel/StabilityAnalysis01.py
+++ PsPartPaperModel/StabilityAnalysis01.py
@@ -27,7 +27,7 @@ sys = Matrix([x0 * (1. - x0 - k * (x1 + x3 + x5)),
 jac = sys.jacobian([x0, x1, x2, x3, x4, x5])
 #genera_solns = nonlinsolve(sys, [x0, x1, x2, x3, x4, x5]) # never finishes
 #solns2 = solve(sys, [x0, x1, x2, x3, x4, x5], simplify=False) # Works
-solns = np.load('../PsPartPaperModel/PsPartFixedPoints.npy')
+solns = np.load('PsPartFixedPoints.npy')
 k_range = np.linspace(-2., 2., 200)
 
 # Trying out a range of k values. Each list will hold the k values for which
@@ -63,24 +63,24 @@ for i in range(len(k_range)):
 # attractors at (I'm pretty sure) k = 1/3. Can't say much about the other
 # fixed points, but this at least is good.
 
-# Trying a numerical method
-# Issue: only approximations, so looking at eigenvals could be misleading...
-#def sys_num(x):
-#    k = 1.0
-#    x0, x1, x2, x3, x4, x5 = x
-#    dx0 = x0 * (1. - x0 - k * (x1 + x3 + x5))
-#    dx1 = x1 * (1. - x1 - k * (x0 + x2 + x4))
-#    dx2 = x2 * (1. - x2 - k * (x1 + x3 + x5))
-#    dx3 = x3 * (1. - x3 - k * (x0 + x2 + x4))
-#    dx4 = x4 * (1. - x4 - k * (x1 + x3 + x5))
-#    dx5 = x5 * (1. - x5 - k * (x0 + x2 + x4))
-#    return(dx0, dx1, dx2, dx3, dx4, dx5)
-#
-#solns = []
-#for i in range(1000):
-##    if i % 10 == 0: print(i)
-#    rand_init = np.random.uniform(-0.5, 1.5, nlinks).tolist()
-#    s = np.round(fsolve(sys_num, rand_init), 2)
-#    if not any((s == x).all() for x in solns):
-#        solns.append(s)
-#
+# Classifying fps for three values of k
+dims = [x0, x1, x2, x3, x4, x5]
+stable = []
+saddles = []
+unstable = []
+nonhyp = []
+ks = [0.5, 1.0, 2.0]
+#ks = [-0.2]
+for kval in range(len(ks)):
+    for fp in range(solns.shape[0]):
+        pts = [i.subs({k: ks[kval]}) for i in solns[fp,]]
+        assign = dict(zip(dims, pts))
+        assign[k] = ks[kval]
+        eig = jac.subs(assign).eigenvals()
+        vals = list(eig.keys())
+        if all(re(i) < 0. for i in vals):
+            print('k = {}, st. fp at {}'.format(ks[kval], pts))
+        elif all(re(i) == 0. for i in vals):
+            print('k = {}, nonhyperbolic fp at {}'.format(ks[kval], pts))
+
+
